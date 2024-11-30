a2721208749fcdcc6d19d842a4a5dbdc9131516b
chenglong.li <chenglong.li007@gmail.com>
2024-11-30 17:33:23 +0800

    Add transfer api

--- api/account.go
+++ api/account.go
@@ -16,7 +16,7 @@ type CreateAccountParams struct {
 
 type CreateAccountRequest struct {
 	Owner    string `json:"owner" binding:"required"`
-	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+	Currency string `json:"currency" binding:"required,currency"`
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
